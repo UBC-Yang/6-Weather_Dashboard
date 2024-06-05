47bf06471665af75dffe6e834c81c3ec7a17b81e
Shu Yang <shuyang@Thomass-MacBook-Pro.local>
2024-06-04 19:08:32 -0600

    current weather update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
 const APIKey = '8652a6bb674d5365b4b3346bfd95a324';
 const APIBase = 'https://api.openweathermap.org/data/2.5/';
+const searchBtn = document.getElementById('searchBtn');
+const city = document.getElementById('search-city');
 
 function getWeather() {
-    const requestUrl = `https://api.openweathermap.org/data/2.5/q=${city}&appid=${APIKey}`;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/q=${city.value}&appid=${APIKey}`;
 
     fetch(requestUrl)
     .then(function (response) {
@@ -10,8 +12,6 @@ function getWeather() {
     })
     .then(function (data) {
         const currentConditions = $('#current-conditions');
-
-        const currentData = data.list[0];
         currentConditions.innerHTML = `
             <h3 id="city-name">${data.name}</h3>
             <p id="date">${new Date().toLocaleDateString()}</p>
@@ -22,4 +22,4 @@ function getWeather() {
     })
 }
 
-searchBtn.on('click', getWeather);
+searchBtn.addEventListener('click', getWeather)
